c2da0ee01b5044d1ba230e5670c8b552859f67d1
Lívia Kelebercová <livia.kelebercova@gmail.com>
2024-03-20 12:30:46 +0100

    kontakt db

---
+++ _inc/config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+//konštanta DATABASE obsahujúca asoc pole 
+define('DATABASE', [
+    'HOST' => 'localhost',
+    'DBNAME' => 'sj_2_2024',
+    'USER_NAME' => 'root',
+    'PASSWORD' => ''
+]);
+
+
+?>
